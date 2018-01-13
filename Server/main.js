d69f86019a42579211d287d9ecbf1c680cf984b4
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-13 16:51:51 +0900

    [add]サーバーを仮実装した
    テスト環境として作っておく必要があった

---
+++ Server/main.js
@@ -0,0 +1,31 @@
+//ライブラリのインポート
+var http = require('http');
+var fs = require('fs');
+
+//サーバーの作成
+let server = http.createServer();
+//サーバーにクライアントがリクエストした時に呼ばれる関数を設定
+server.on("request", routing);
+//サーバーポート設定
+server.listen(3000);
+
+//ルーティングをして適切にデータをクライアントに送信
+function routing(req, res) {
+    switch (req.url) {
+        case "/":
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(fs.readFileSync("./Html/login.html"));
+            break;
+        case "/Css/login.css":
+            res.writeHead(200, {
+                'Content-Type': 'text/css'
+            });
+            res.end(fs.readFileSync("./Css/login.css"));
+            break;
+        default:
+            res.end("not find url " + req.url);
+            break;
+    }
+}
