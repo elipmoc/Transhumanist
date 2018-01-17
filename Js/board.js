71e6641c1ff4d0d2bb7f6b4d3c841c477dafb599
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-17 14:51:35 +0900

    [fix]背景スプライト生成を関数に分離
    見づらかったから

--- Js/board.js
+++ Js/board.js
@@ -1,23 +1,22 @@
 enchant();
 
+function createBackColorSprite(width, height) {
+    let backColorSprite = new Sprite(width, height);
+    let surface = new Surface(width, height);
+    surface.context.beginPath();
+    surface.context.fillStyle = "rgb(0,0,0)";
+    surface.context.fillRect(0, 0, width, height);
+    backColorSprite.image = surface;
+    return backColorSprite;
+}
+
 window.onload = () => {
 
-    var game = new Game(960, 876);
+    let game = new Game(960, 876);
     game.onload = function () {
         // ここに処理を書いていきます。
-
-
         var scene = new Scene();
-        let backColorSprite = new Sprite(960, 876);
-        let surface = new Surface(960, 876)
-        surface.context.beginPath();
-        surface.context.fillStyle = "rgb(0,0,0)";
-        surface.context.fillRect(0, 0, 960, 876);
-        backColorSprite.image = surface;
-        scene.addChild(backColorSprite);
-        let label = new Label("hello! world!");
-        label.font = 'bold 30px "ＭＳ 明朝"';
-        scene.addChild(label);
+        scene.addChild(createBackColorSprite(960, 876));
         game.pushScene(scene);
     };
     game.start();
