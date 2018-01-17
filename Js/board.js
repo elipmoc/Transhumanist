427cd90be88ae6ff4ef32f04cd7971cd1513ad8b
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-17 15:22:24 +0900

    [fix]cssでBoard画面の配置を調整

--- Js/board.js
+++ Js/board.js
@@ -18,6 +18,15 @@ window.onload = () => {
         var scene = new Scene();
         scene.addChild(createBackColorSprite(960, 876));
         game.pushScene(scene);
+        let box = new Sprite(20, 20);
+        let surface = new Surface(20, 20);
+        surface.context.beginPath();
+        surface.context.fillStyle = "rgb(0,200,200)";
+        surface.context.fillRect(0, 0, 20, 20);
+        box.image = surface;
+        box.addEventListener("touchstart", function (e) { alert("touch"); });
+        scene.addChild(box);
+
     };
     game.start();
 }
