a11fbe520d9a5e1b5077883f3207a589702e2436
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-20 01:41:01 +0900

    [update]プレイヤーの枠をテスト表示
    プレイヤー画像を描画できるかの練習

--- Js/board.js
+++ Js/board.js
@@ -2,9 +2,15 @@
 window.onload = () => {
     let stage = new createjs.Stage("myCanvas");
     let background = new createjs.Shape();
-    background.graphics.beginFill("black").drawRect(0, 0, 960, 876);
+    background.graphics.beginFill("black").
+        drawRect(0, 0, stage.canvas.width, stage.canvas.height);
     background.x = 0;
     background.y = 0;
     stage.addChild(background);
+    var bmp = new createjs.Bitmap("Img/ui/evenPlayerFrame.png");
+    bmp.regX = bmp.image.width / 2;
+    bmp.regY = 0;
+    bmp.x = stage.canvas.width / 2;
+    stage.addChild(bmp);
     stage.update();
 }
