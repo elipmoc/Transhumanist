816b7e19c93106e6e5b87fa2bf68931315bd59a2
Sutaa <sutaahbts@yahoo.co.jp>
2018-01-20 23:17:32 +0900

    [update]ゲーム画面に画像を描画。
    board.jsに画面表示に必要なUI表示を追加。

--- Js/board.js
+++ Js/board.js
@@ -1,16 +1,100 @@
+const queue = new createjs.LoadQueue();
 
 window.onload = () => {
+    queue.on("complete",preloadImage,this);
+    queue.loadManifest([
+        {id:"evenPlayerFrame",src:"Img/ui/evenPlayerFrame.png"},
+        {id:"evenPlayerFrame2",src:"Img/ui/evenPlayerFrame2.png"},
+        {id:"oddPlayerFrame",src:"Img/ui/oddPlayerFrame.png"},
+        {id:"oddPlayerFrame2",src:"Img/ui/oddPlayerFrame2.png"},
+        {id:"topWindows",src:"Img/ui/topWindows.png"},
+        {id:"setting",src:"Img/ui/setting.png"},
+        {id:"evenPlayerRBArea",src:"Img/ui/evenPlayerRBArea.png"},
+        {id:"oddPlayerRBArea",src:"Img/ui/oddPlayerRBArea.png"},
+        {id:"logEvent",src:"Img/ui/logEvent.png"},
+        {id:"button",src:"Img/ui/button.png"},
+        {id:"handTest",src:"Img/ui/handTest.png"}
+    ]);
+}
+
+function preloadImage(){
     let stage = new createjs.Stage("myCanvas");
     let background = new createjs.Shape();
     background.graphics.beginFill("black").
         drawRect(0, 0, stage.canvas.width, stage.canvas.height);
-    background.x = 0;
-    background.y = 0;
     stage.addChild(background);
-    var bmp = new createjs.Bitmap("Img/ui/evenPlayerFrame.png");
-    bmp.regX = bmp.image.width / 2;
-    bmp.regY = 0;
-    bmp.x = stage.canvas.width / 2;
-    stage.addChild(bmp);
+
+    //プレイヤー1の枠
+    let player1frame = new createjs.Bitmap();
+    player1frame.image = queue.getResult("evenPlayerFrame");
+    player1frame.regX = player1frame.image.width / 2;
+    player1frame.regY = player1frame.image.height;
+    player1frame.x = stage.canvas.width / 2;
+    player1frame.y = stage.canvas.height;
+    stage.addChild(player1frame);
+    //プレイヤー1の設置アクション
+    let player1buildArea = new createjs.Bitmap();
+    player1buildArea.image = queue.getResult("oddPlayerRBArea");
+    player1buildArea.regX = player1buildArea.image.width / 2;
+    player1buildArea.regY = player1buildArea.image.height;
+    player1buildArea.x = stage.canvas.width / 2;
+    player1buildArea.y = stage.canvas.height - player1frame.image.height - 4;
+    stage.addChild(player1buildArea);
+    //プレイヤー1のリソース
+    let player1resourceArea = new createjs.Bitmap();
+    player1resourceArea.image = queue.getResult("oddPlayerRBArea");
+    player1resourceArea.regX = player1resourceArea.image.width / 2;
+    player1resourceArea.regY = player1resourceArea.image.height;
+    player1resourceArea.x = stage.canvas.width / 2;
+    player1resourceArea.y = player1buildArea.y - player1buildArea.image.height - 4;
+    stage.addChild(player1resourceArea);
+
+    //プレイヤー2の枠
+    let player2frame = new createjs.Bitmap();
+    player2frame.image = queue.getResult("oddPlayerFrame");
+    player2frame.regY = player2frame.image.height / 2;
+    player2frame.y = stage.canvas.height / 2;
+    stage.addChild(player2frame);
+
+    //プレイヤー3の枠
+    let player3frame = new createjs.Bitmap();
+    player3frame.image = queue.getResult("evenPlayerFrame");
+    player3frame.rotation = 180;
+    player3frame.regX = player3frame.image.width / 2;
+    player3frame.regY = 0;
+    player3frame.x = stage.canvas.width / 2;
+    player3frame.y = player3frame.image.height;
+    stage.addChild(player3frame);
+
+    //プレイヤー4の枠
+    let player4frame = new createjs.Bitmap();
+    player4frame.image = queue.getResult("oddPlayerFrame");
+    player4frame.rotation = 180;
+    player4frame.regX = player4frame.image.width;
+    player4frame.regY = player4frame.image.height / 2;
+    player4frame.x = stage.canvas.width - player4frame.image.width;
+    player4frame.y = stage.canvas.height / 2;
+    stage.addChild(player4frame);
+
+    //設定枠
+    let topWindowsL = new createjs.Bitmap();
+    topWindowsL.image = queue.getResult("topWindows");
+    stage.addChild(topWindowsL);
+    //設定ボタン
+    let setting = new createjs.Bitmap();
+    setting.image = queue.getResult("setting");
+    setting.x = (topWindowsL.image.height - setting.image.height)/2;
+    setting.y = (topWindowsL.image.height - setting.image.height)/2;
+    stage.addChild(setting);
+
+    //イベント枠
+    let topWindowsR = new createjs.Bitmap();
+    topWindowsR.image = queue.getResult("topWindows");
+    topWindowsR.scaleX = -1;
+    topWindowsR.x = stage.canvas.width;
+    stage.addChild(topWindowsR);
+
+
+
     stage.update();
 }
