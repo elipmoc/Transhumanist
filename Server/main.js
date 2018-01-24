6f2af6b7fbfb9103abfa1b497a28dc871ebfccc7
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-25 00:23:19 +0900

    [update]ソケットを作成した

--- Server/main.js
+++ Server/main.js
@@ -1,9 +1,12 @@
 //ライブラリのインポート
 const http = require('http');
 const myRouter = require('./routing.js');
+const io = require("socket.io");
 
 //サーバーの作成
 let server = http.createServer();
+const socket = io(server);
+
 //サーバーにクライアントがリクエストした時に呼ばれる関数を設定
 server.on("request", myRouter.createRouter());
 //サーバーポート設定
