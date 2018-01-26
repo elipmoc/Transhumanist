f08b1c780d8930179c36d6eb48399faba97b5080
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-27 00:30:06 +0900

    [update]view.tsをインポートするようにした

--- Ts/board.ts
+++ Ts/board.ts
@@ -1,7 +1,10 @@
+import * as view from "./view"
+
 const queue = new createjs.LoadQueue();
 const canvasWidth = 960;
 const canvasHeight = 876;
 window.onload = () => {
+    new view.PlayerInfo();
     queue.on("complete", preloadImage);
     queue.loadManifest([
         { id: "evenPlayerFrame", src: "Img/ui/evenPlayerFrame.png" },
