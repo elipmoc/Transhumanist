6f71b28288d811d04d9dda38f30c749a99e37cbf
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-26 23:40:11 +0900

    [update]PlayerInfoクラス作成

--- Ts/view.ts
+++ Ts/view.ts
@@ -1,6 +1,11 @@
-/*class Button {
-    private onClicked: () => void;
-    constructor(func: () => void) {
-        this.onClicked = func;
-    }
-}*/
+
+//player情報
+class PlayerInfo {
+    playerName: string;
+    speed: number;
+    resource: number;
+    activityRange: number;
+    uncertainty: number;
+    positive: number;
+    negative: number;
+}
