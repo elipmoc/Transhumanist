932aae67e866914ba3f747528e6b8f1b5d0248d2
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-02-19 01:21:15 +0900

    [refactoring]boardGlobalData.tsにcanvasのサイズ移行作業

--- Client/board.ts
+++ Client/board.ts
@@ -1,10 +1,8 @@
+import * as global from "./boardGlobalData"
 import * as view from "./view"
 
 const queue = new createjs.LoadQueue();
-const canvasWidth = 960;
-const canvasHeight = 876;
 window.onload = () => {
-    new view.Player1Window();
 
     queue.on("complete", preloadImage);
     queue.loadManifest([
@@ -26,35 +24,35 @@ function preloadImage() {
     let stage = new createjs.Stage("myCanvas");
     let background = new createjs.Shape();
     background.graphics.beginFill("black").
-        drawRect(0, 0, canvasWidth, canvasHeight);
+        drawRect(0, 0, global.canvasWidth, global.canvasHeight);
     stage.addChild(background);
 
     //プレイヤー1の枠
     let player1frame = new createjs.Bitmap(queue.getResult("evenPlayerFrame"));
     player1frame.regX = player1frame.image.width / 2;
     player1frame.regY = player1frame.image.height;
-    player1frame.x = canvasWidth / 2;
-    player1frame.y = canvasHeight;
+    player1frame.x = global.canvasWidth / 2;
+    player1frame.y = global.canvasHeight;
     stage.addChild(player1frame);
     //プレイヤー1の設置アクション
     let player1buildArea = new createjs.Bitmap(queue.getResult("oddPlayerRBArea"));
     player1buildArea.regX = player1buildArea.image.width / 2;
     player1buildArea.regY = player1buildArea.image.height;
-    player1buildArea.x = canvasWidth / 2;
-    player1buildArea.y = canvasHeight - player1frame.image.height - 4;
+    player1buildArea.x = global.canvasWidth / 2;
+    player1buildArea.y = global.canvasHeight - player1frame.image.height - 4;
     stage.addChild(player1buildArea);
     //プレイヤー1のリソース
     let player1resourceArea = new createjs.Bitmap(queue.getResult("oddPlayerRBArea"));
     player1resourceArea.regX = player1resourceArea.image.width / 2;
     player1resourceArea.regY = player1resourceArea.image.height;
-    player1resourceArea.x = canvasWidth / 2;
+    player1resourceArea.x = global.canvasWidth / 2;
     player1resourceArea.y = player1buildArea.y - player1buildArea.image.height - 4;
     stage.addChild(player1resourceArea);
 
     //プレイヤー2の枠
     let player2frame = new createjs.Bitmap(queue.getResult("oddPlayerFrame"));
     player2frame.regY = player2frame.image.height / 2;
-    player2frame.y = canvasHeight / 2;
+    player2frame.y = global.canvasHeight / 2;
     stage.addChild(player2frame);
 
     //プレイヤー3の枠
@@ -62,7 +60,7 @@ function preloadImage() {
     player3frame.rotation = 180;
     player3frame.regX = player3frame.image.width / 2;
     player3frame.regY = 0;
-    player3frame.x = canvasWidth / 2;
+    player3frame.x = global.canvasWidth / 2;
     player3frame.y = player3frame.image.height;
     stage.addChild(player3frame);
 
@@ -71,8 +69,8 @@ function preloadImage() {
     player4frame.rotation = 180;
     player4frame.regX = player4frame.image.width;
     player4frame.regY = player4frame.image.height / 2;
-    player4frame.x = canvasWidth - player4frame.image.width;
-    player4frame.y = canvasHeight / 2;
+    player4frame.x = global.canvasWidth - player4frame.image.width;
+    player4frame.y = global.canvasHeight / 2;
     stage.addChild(player4frame);
 
     //設定枠
@@ -87,15 +85,15 @@ function preloadImage() {
     //イベント枠
     let topWindowsR = new createjs.Bitmap(queue.getResult("topWindows"));
     topWindowsR.scaleX = -1;
-    topWindowsR.x = canvasWidth;
+    topWindowsR.x = global.canvasWidth;
     stage.addChild(topWindowsR);
 
     //ターン終了ボタン
     let turnFinishButton = new createjs.Bitmap(queue.getResult("button"));
     turnFinishButton.regX = turnFinishButton.image.width;
     turnFinishButton.regY = turnFinishButton.image.height;
-    turnFinishButton.x = canvasWidth - 20;
-    turnFinishButton.y = canvasHeight - 20;
+    turnFinishButton.x = global.canvasWidth - 20;
+    turnFinishButton.y = global.canvasHeight - 20;
     stage.addChild(turnFinishButton);
     //ターン終了ボタンテキスト
     let turnFinishText = new createjs.Text("ターン終了", "20px Arial");
@@ -110,7 +108,7 @@ function preloadImage() {
     declareWarButton.regX = 0;
     declareWarButton.regY = declareWarButton.image.height;
     declareWarButton.x = 20;
-    declareWarButton.y = canvasHeight - 20;
+    declareWarButton.y = global.canvasHeight - 20;
     stage.addChild(declareWarButton);
 
     //宣戦布告ボタンテキスト
@@ -120,7 +118,6 @@ function preloadImage() {
     declareWarText.x = declareWarButton.x + declareWarButton.image.width / 2;
     declareWarText.y = declareWarButton.y - declareWarButton.image.height / 2;
     stage.addChild(declareWarText);
-
 
     stage.update();
 }
