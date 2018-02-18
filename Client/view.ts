1c7c3088a350c9b26764ff75e19ea0aaafb3d6fd
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-02-19 01:33:28 +0900

    [update]プレイヤー1のフレームをview.tsでクラス化

--- Client/view.ts
+++ Client/view.ts
@@ -1,4 +1,4 @@
-
+import * as global from "./boardGlobalData"
 //player情報
 export class PlayerInfo {
     playerName: string;
@@ -20,16 +20,33 @@ export class PlayerWindowBase {
     protected positive: createjs.Text;
     protected negative: createjs.Text;
     protected playerFrame: createjs.Bitmap;
-    constructor() {
+    protected group: createjs.Container;
+    constructor(parent: createjs.Container) {
+        this.group = new createjs.Container();
         this.playerNameText = new createjs.Text();
-
+        this.playerFrame = new createjs.Bitmap("");
+        this.group.addChild(this.playerFrame);
+        this.group.addChild(this.playerNameText);
+        parent.addChild(this.group);
     }
     //set PlayerInfo(playerInfo: PlayerInfo) {
 }
 
 export class Player1Window extends PlayerWindowBase {
-    constructor() {
-        super();
-        this.negative.color = "";
+    constructor(parent: createjs.Container, queue: createjs.LoadQueue) {
+        super(parent);
+        this.playerFrame.image = <any>queue.getResult("evenPlayerFrame");
+        this.playerFrame.regX = this.playerFrame.image.width / 2;
+        this.playerFrame.regY = 0;
+        this.group.x = global.canvasWidth / 2;
+        this.group.y = global.canvasHeight - this.playerFrame.image.height;
+        this.playerNameText.color = "blue";
+        this.playerNameText.text = "輝夜月";
+        this.playerNameText.font = "20px Arial";
+        this.playerNameText.regX = this.playerNameText.getMeasuredWidth() / 2;
+        this.playerNameText.x;
+        this.playerNameText.y = 5;
+
+
     }
 }
