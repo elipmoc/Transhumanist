94b56d1bd7d99043f541e9e06d9bd1e43b261ce8
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-03-21 00:25:57 +0900

    [update]RoomDataの実装を進めた

--- Server/roomData.ts
+++ Server/roomData.ts
@@ -1,5 +1,6 @@
 import { RoomEvents } from "../Server/roomEvents";
 import { PlayerData } from "../Server/playerData";
+import { PlayFlagDataForClient } from "../Share/playFlagDataForClient";
 
 export class RoomData {
     private roomId: number;
@@ -10,15 +11,37 @@ export class RoomData {
     private password: string;
     private roomEvents: RoomEvents;
 
-    constructor(roomId: number, roomName: string, password: string, roomEvents: RoomEvents) { }
+    constructor(roomId: number, roomName: string, password: string, roomEvents: RoomEvents) {
+        this.roomId = roomId;
+        this.roomName = roomName;
+        this.password = password;
+        this.passwordFlag = (password == "");
+        this.playFlag = false;
+        this.roomEvents = roomEvents;
+    }
+
     getRoomId() { return this.roomId; }
+
     getRoomName() { return this.roomName; }
+
     getPlayFlag() { return this.playFlag; }
-    setPlayFlag(playFlag: boolean) { }
+
+    setPlayFlag(playFlag: boolean) {
+        this.playFlag = playFlag;
+        let playFlagDataForClient: PlayFlagDataForClient
+            = { playFlag: playFlag, roomId: this.roomId };
+        this.roomEvents.updatePlayFlagCallBack(playFlagDataForClient);
+    }
+
     exsistUuid(uuid: number) { return true; }
+
     deleteMember(uuid: number) { }
+
     addMember(playerData: PlayerData) { }
-    passwordCheck(str: string) { return true; }
-    needPassword() { return true; }
-    deleteRoom() { }
+
+    passwordCheck(str: string) { return this.password == str; }
+
+    needPassword() { return this.passwordFlag; }
+
+    deleteRoom() { this.roomEvents.deleteRoomCallBack(this.roomId); }
 }
