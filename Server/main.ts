9702bca87e86e332e561512e1c8e54ebeb21aa9d
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-03-24 02:24:03 +0900

    [change]main.ts内で、loginSocket・loginControlerの作成の定義。

--- Server/main.ts
+++ Server/main.ts
@@ -2,6 +2,8 @@
 import * as http from 'http';
 const myRouter = require('./routing.js');
 import * as io from "socket.io";
+import {LoginSocket} from "../Server/loginSocket";
+import {LoginControler} from "../Server/loginControler";
 
 //サーバーの作成
 let server = http.createServer();
@@ -11,7 +13,8 @@ server.on("request", myRouter.createRouter());
 //メインソケットを作成
 const mainSocket = io(server);
 //メインソケットからログインソケットを作成
-const loginSocket = require("./loginSocket.js").create(mainSocket);
+const loginControler = new LoginControler;
+const loginSocket = new LoginSocket(mainSocket,loginControler);
 
 //サーバーポート設定
 server.listen(3000);
