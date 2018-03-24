27689cbd8c2164e5b3ff9ed918156c66cca68004
elipmoc <relipmoc101@gmail.com>
2018-03-24 15:47:02 +0900

    [fix]exsistUuidメソッドを削除し代わりにgetPlayerDataメソッドを追加

--- Server/roomData.ts
+++ Server/roomData.ts
@@ -33,7 +33,7 @@ export class RoomData {
         this.roomEvents.updatePlayFlagCallBack(playFlagDataForClient);
     }
 
-    exsistUuid(uuid: string) { return true; }
+    getPlayerData(uuid: string) { }
 
     deleteMember(uuid: string) { }
 
