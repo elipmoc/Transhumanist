956af8e0a533534e1bf0194cb9fe0fc37e0b55c1
elipmoc <relipmoc101@gmail.com>
2018-03-26 16:42:45 +0900

    [update]routing.jsをts化した

--- Server/main.ts
+++ Server/main.ts
@@ -1,24 +1,24 @@
 //ライブラリのインポート
 import * as http from 'http';
-const myRouter = require('./routing.js');
+import { createRouter } from "./routing";
 import * as io from "socket.io";
-import {LoginControler} from "../Server/loginControler";
-import {LoginSocket} from "../Server/loginSocket";
-import {BoardControler} from "../Server/boardControler";
-import {BoardSocket} from "../Server/boardSocket";
+import { LoginControler } from "../Server/loginControler";
+import { LoginSocket } from "../Server/loginSocket";
+import { BoardControler } from "../Server/boardControler";
+import { BoardSocket } from "../Server/boardSocket";
 
 //サーバーの作成
 let server = http.createServer();
 
 //サーバーにクライアントがリクエストした時に呼ばれる関数を設定
-server.on("request", myRouter.createRouter());
+server.on("request", createRouter());
 //メインソケットを作成
 const mainSocket = io(server);
 //メインソケットからログインソケットを作成
 const loginControler = new LoginControler;
 const boardControler = new BoardControler;
-const loginSocket = new LoginSocket(mainSocket,loginControler);
-const boardSocket = new BoardSocket(mainSocket,boardControler);
+const loginSocket = new LoginSocket(mainSocket, loginControler);
+const boardSocket = new BoardSocket(mainSocket, boardControler);
 
 //サーバーポート設定
 server.listen(3000);
---
+++ Server/routing.ts
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const router = require('router');
+const finalhandler = require('finalhandler');
+
+export function createRouter() {
+    let myRouter = router();
+    myRouter.get("/", (req: any, res: any) => {
+        sendHtml(res, "./Html/login.html");
+    });
+    myRouter.get("/Client/Lib/easel.js", (req: any, res: any) => {
+        sendHtml(res, "./Client/Lib/easeljs-0.8.2.min.js");
+    });
+    myRouter.get("/Client/Lib/preload.js", (req: any, res: any) => {
+        sendHtml(res, "./Client/Lib/preloadjs-0.6.2.min.js");
+    });
+    myRouter.get("/Css/:path", (req: any, res: any) => {
+        sendCss(res, "./Css/" + req.params.path);
+    });
+    myRouter.get("/Client/:path", (req: any, res: any) => {
+        sendJs(res, "./Client/" + req.params.path);
+    });
+    myRouter.get("/Img/:path", (req: any, res: any) => {
+        sendPng(res, "./Resource/Img/" + req.params.path);
+    });
+    myRouter.get("/Img/ui/:path", (req: any, res: any) => {
+        sendPng(res, "./Resource/Img/ui/" + req.params.path);
+    });
+    myRouter.get("/:path", (req: any, res: any) => {
+        sendHtml(res, "./Html/" + req.params.path);
+    });
+
+    return (req: any, res: any) => {
+        myRouter(req, res, finalhandler(req, res));
+    }
+}
+
+function sendHtml(res: any, path: string) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(fs.readFileSync(path));
+}
+
+function sendCss(res: any, path: string) {
+    res.writeHead(200, {
+        'Content-Type': 'text/css'
+    });
+    res.end(fs.readFileSync(path));
+}
+
+function sendJs(res: any, path: string) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plane'
+    });
+    res.end(fs.readFileSync(path));
+}
+function sendPng(res: any, path: string) {
+    res.writeHead(200, {
+        'Content-Type': 'image/png'
+    });
+    res.end(fs.readFileSync(path));
+}
