a3b19f9ad6b3a83d4d825d7a6ac1fe4935e83777
elipmoc <relipmoc101@gmail.com>
2018-04-01 15:30:57 +0900

    [update]OptionWindowにテキストやアイコン等の配置をした

--- Client/view.ts
+++ Client/view.ts
@@ -270,13 +270,63 @@ export class Player4Window extends viewBase.PlayerWindowBase {
 export class OptionWindow extends createjs.Container {
     constructor(queue: createjs.LoadQueue) {
         super();
+
         const optionFrame = new createjs.Bitmap(queue.getResult("optionWindow"));
         optionFrame.regX = optionFrame.image.width / 2;
         optionFrame.regY = optionFrame.image.height / 2;
         this.addChild(optionFrame);
+
         const optionCrossButton = new OptionCrossButton(() => { this.visible = false; this.stage.update(); }, queue);
         optionCrossButton.x = 200;
         optionCrossButton.y = - 270;
         this.addChild(optionCrossButton);
+
+        const optionText = new createjs.Text();
+        optionText.x = -180;
+        optionText.y = -260;
+        optionText.text = "オプション";
+        optionText.color = "white";
+        optionText.font = "45px Arial";
+        this.addChild(optionText);
+
+        const optionIcon = new createjs.Bitmap(queue.getResult("setting"));
+        optionIcon.scaleX = 2;
+        optionIcon.scaleY = 2;
+        optionIcon.x = -280;
+        optionIcon.y = -280;
+        this.addChild(optionIcon);
+
+        const volumeText = new createjs.Text();
+        volumeText.x = -270;
+        volumeText.y = -180;
+        volumeText.text = "Volume";
+        volumeText.color = "white";
+        volumeText.font = "45px Arial";
+        this.addChild(volumeText);
+
+        const BgmText = new createjs.Text();
+        BgmText.x = -270;
+        BgmText.y = -130;
+        BgmText.text = "BGM";
+        BgmText.color = "white";
+        BgmText.font = "30px Arial";
+        this.addChild(BgmText);
+
+        const SeText = new createjs.Text();
+        SeText.x = -270;
+        SeText.y = -90;
+        SeText.text = "SE";
+        SeText.color = "white";
+        SeText.font = "30px Arial";
+        this.addChild(SeText);
+
+        const developerText = new createjs.Text();
+        developerText.x = -40;
+        developerText.y = -40;
+        developerText.text = "開発スタッフ";
+        developerText.color = "white";
+        developerText.font = "30px Arial";
+        this.addChild(developerText);
+
     }
 }
