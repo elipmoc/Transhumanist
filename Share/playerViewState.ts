c1c96e42b6f2865c456ccc604e83ca22249a3349
elipmoc <relipmoc101@gmail.com>
2018-04-21 15:54:16 +0900

    [add]playerViewState.ts
    ボドゲのプレイヤーステータス表示を更新するためのデータとして使う

---
+++ Share/playerViewState.ts
@@ -0,0 +1,9 @@
+export interface PlayerViewState {
+    playerName: string;
+    speed: number;
+    resource: number;
+    activityRange: number;
+    uncertainty: number;
+    positive: number;
+    negative: number;
+}
