8bddff6f52b927c750a42419ea8ace82e4d429dd
elipmoc <relipmoc101@gmail.com>
2018-04-28 15:56:45 +0900

    [update]SelectActionWindowに枚数を表示

--- Client/SelectActionWindow.ts
+++ Client/SelectActionWindow.ts
@@ -1,12 +1,46 @@
 import * as global from "./boardGlobalData"
+import { NumberOfActionCard } from "../Share/numberOfActionCard";
+
+class NumberOfActionCardTexts extends createjs.Container {
+    private texts: createjs.Text[] = new Array();
+
+    constructor() {
+        super();
+        const maxLevel = 6;
+        const widthInterval = 105;
+        for (let i = 0; i < maxLevel; i++) {
+            const text = new createjs.Text();
+            text.color = "white";
+            text.font = "16px Bold ＭＳ ゴシック";
+            text.textAlign = "center";
+            text.text = "99/99\n(99)";
+            text.x = widthInterval * i;
+            this.addChild(text);
+            this.texts.push(text);
+        }
+    }
+
+    setNumberOfActionCard(numberOfActionCardList: NumberOfActionCard[]) {
+        for (let i = 0; i < numberOfActionCardList.length; i++) {
+            this.texts[i].text =
+                numberOfActionCardList[i].currentNumber +
+                "/" +
+                numberOfActionCardList[i].maxNumber +
+                "\n(" +
+                numberOfActionCardList[i].dustNumber + ")";
+        }
+    }
+}
 
 //獲得するアクションカードを選択するウインドウ
 export class SelectActionWindow extends createjs.Container {
 
     private callBack: (value: number) => void;
+    private numberOfActionCardTexts: NumberOfActionCardTexts;
 
     constructor(queue: createjs.LoadQueue) {
         super();
+
         const frame = new createjs.Shape();
         const frameX = 700;
         const frameY = 250;
@@ -15,6 +49,10 @@ export class SelectActionWindow extends createjs.Container {
         frame.alpha = 0.5;
         frame.x = global.canvasWidth / 2 - frameX / 2;
         frame.y = global.canvasHeight / 2 - frameY / 2;
+
+        this.numberOfActionCardTexts = new NumberOfActionCardTexts();
+        this.numberOfActionCardTexts.x = global.canvasWidth / 2 - frameX / 2 + 90;
+        this.numberOfActionCardTexts.y = global.canvasHeight / 2 + frameY / 2 - 30;
 
         const descriptionText = new createjs.Text();
         descriptionText.textAlign = "center";
@@ -26,6 +64,7 @@ export class SelectActionWindow extends createjs.Container {
 
         this.addChild(frame);
         this.addChild(descriptionText);
+        this.addChild(this.numberOfActionCardTexts);
 
         for (var i = 1; i <= 3; i++) {
             const level = new createjs.Bitmap(queue.getResult("level" + (4 - i)));
