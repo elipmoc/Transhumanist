90f570c20bf88651fc5b43eeb1f4531f36e9bcca
elipmoc <relipmoc101@gmail.com>
2018-05-03 12:37:55 +0900

    [update]resource.pngをqueueに登録

--- Client/board.ts
+++ Client/board.ts
@@ -30,6 +30,7 @@ window.onload = () => {
         { id: "level4", src: "Img/card/back/level4mb.png" },
         { id: "level5", src: "Img/card/back/level5mb.png" },
         { id: "level6", src: "Img/card/back/level6mb.png" },
+        { id: "resource", src: "Img/resource.png" }
     ]);
 }
 
