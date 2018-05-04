ccf694cc6440cd57e4c17b01a7c5e48e4a4dd64c
elipmoc <relipmoc101@gmail.com>
2018-05-04 12:31:15 +0900

    [add]enum ResourceKind

---
+++ Share/resourceKind.ts
@@ -0,0 +1,22 @@
+export const enum ResourceKind {
+    human = 0,//人間
+    sick_human,//病人
+    metal,//メタル
+    gas,//ガス
+    Silicon,//ケイ素
+    sulfur,//硫黄
+    bible,//聖書
+    syringe,//注射器
+    cpu,//CPU
+    bomb,//爆弾
+    microchip,//マイクロチップ
+    rocket,//ロケット
+    genetic_data,//遺伝子データ
+    believer,//信者
+    information_on_Mars,//火星の情報
+    satellite,//衛星
+    robot,//ロボット
+    extended_human,//拡張人間
+    terra_forming,//テラフォーミング
+    theological_body//神体
+}
