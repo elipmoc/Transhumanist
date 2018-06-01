676917c56f467fd76c69fce11511cd5cfc224f7e
elipmoc <relipmoc101@gmail.com>
2018-06-01 14:31:09 +0900

    [add]buildActionKindを追加

--- Share/resourceKind.ts
+++ Share/resourceKind.ts
@@ -1,23 +1,23 @@
 export const enum ResourceKind {
-    human = 0,//人間
-    sick_human,//病人
-    metal,//メタル
-    gas,//ガス
-    Silicon,//ケイ素
-    sulfur,//硫黄
-    bible,//聖書
-    syringe,//注射器
-    cpu,//CPU
-    bomb,//爆弾
-    believer,//信者
-    microchip,//マイクロチップ
-    rocket,//ロケット
-    genetic_data,//遺伝子データ
-    information_on_Mars,//火星の情報
-    satellite,//衛星
-    robot,//ロボット
-    extended_human,//拡張人間
-    terra_forming,//テラフォーミング
-    theological_body,//神体
-    none//リソースなし
+    human = 0,              //人間
+    sick_human,             //病人
+    metal,                  //メタル
+    gas,                    //ガス
+    Silicon,                //ケイ素
+    sulfur,                 //硫黄
+    bible,                  //聖書
+    syringe,                //注射器
+    cpu,                    //CPU
+    bomb,                   //爆弾
+    believer,               //信者
+    microchip,              //マイクロチップ
+    rocket,                 //ロケット
+    genetic_data,           //遺伝子データ
+    information_on_Mars,    //火星の情報
+    satellite,              //衛星
+    robot,                  //ロボット
+    extended_human,         //拡張人間
+    terra_forming,          //テラフォーミング
+    theological_body,       //神体
+    none                    //何もなし
 }
