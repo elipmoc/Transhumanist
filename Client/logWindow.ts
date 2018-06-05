a567deafdc19dfb52ecb1d5098a3790ce95eeefe
elipmoc <relipmoc101@gmail.com>
2018-06-05 12:49:40 +0900

    [update]ログのスクロールに制限をかけた

--- Client/logWindow.ts
+++ Client/logWindow.ts
@@ -8,9 +8,9 @@ export class LogWindow extends createjs.Container {
         this.logFrame = new createjs.Bitmap(queue.getResult("logFrame"));
         this.logFrame.x = global.canvasWidth / 2 - this.logFrame.image.width / 2;
         this.logFrame.y = global.canvasHeight / 2 - this.logFrame.image.height / 2;
-        const logMessageBox = new LogMessageBox();
-        logMessageBox.x = this.logFrame.x + 15;
-        logMessageBox.y = this.logFrame.y;
+        const logMessageBox = new LogMessageBox(this.logFrame.image.height);
+        logMessageBox.regX = -this.logFrame.x - 15;
+        logMessageBox.regY = -this.logFrame.y;
         logMessageBox.addMessage("aabb");
         logMessageBox.addMessage("ggghhhgh");
         logMessageBox.addMessage("L知ってるか");
@@ -32,7 +32,7 @@ class LogMessageBox extends createjs.Container {
     private mouseInFlag = false;
     set MouseInFlag(x: boolean) { this.mouseInFlag = x; }
 
-    constructor() {
+    constructor(height: number) {
         super();
         const mouseWheel = (e: MouseWheelEvent) => {
             if (this.mouseInFlag) {
@@ -40,6 +40,7 @@ class LogMessageBox extends createjs.Container {
                     this.y += e.wheelDelta / 10;
                 else
                     this.y -= e.detail * 2.7;
+                this.y = Math.min(height - 10, Math.max(-this.bottomY + 10, this.y));
                 this.stage.update();
                 e.preventDefault();
             }
