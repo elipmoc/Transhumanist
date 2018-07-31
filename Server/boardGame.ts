36493948fac5126776e22968d256ede1c5d56cd4
elipmoc <relipmoc101@gmail.com>
2018-07-31 15:15:15 +0900

    [update] ようこそをログに無限に吐くのやめた

--- Server/boardGame.ts
+++ Server/boardGame.ts
@@ -31,7 +31,7 @@ export class BoardGame {
         this.logMessageList.push(new LogMessageForClient("N.Hのターンです。", LogMessageType.Player2Msg));
         this.logMessageList.push(new LogMessageForClient("らいぱん鳥のターンです。", LogMessageType.Player3Msg));
         this.logMessageList.push(new LogMessageForClient("戦争状態のため、Positiveが-1されました", LogMessageType.Player3Msg));
-        setInterval(() => this.logMessageList.push(new LogMessageForClient("ようこそ", LogMessageType.EventMsg)), 5000);
+        setTimeout(() => this.logMessageList.push(new LogMessageForClient("ようこそ", LogMessageType.EventMsg)), 5000);
         this.eventLogMessage = new SocketBinder<EventLogMessageForClient>("eventLogMessage", this.boardSocket);
         this.eventLogMessage.Value = new EventLogMessageForClient("イベント【人口爆発】が発生しました", "リソース欄にある『人間の』2倍の\n新たな『人間』を追加する。\n新たに追加する時、『人間』は削除対象に出来ない。");
     }
