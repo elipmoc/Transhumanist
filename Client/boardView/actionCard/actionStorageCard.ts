70f02b39f6b9ca94a8f15067bd7d5426f3808e6f
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-08-06 02:24:38 +0900

    [update]手札の表示、完全にしました。

--- Client/boardView/actionCard/actionStorageCard.ts
+++ Client/boardView/actionCard/actionStorageCard.ts
@@ -6,6 +6,9 @@ export class ActionStorageCard extends createjs.Container {
     private cardFrame: createjs.Bitmap;
     private cardImage: createjs.Bitmap;
     private cardName: createjs.Text;
+    private cardCap: createjs.Text;
+    private cardLevel: createjs.Text;
+    private cardType: createjs.Text;
     private yamlData: ActionCardYamlData = null;
     private index: number
     readonly width: number = 84;
@@ -27,18 +30,37 @@ export class ActionStorageCard extends createjs.Container {
         this.cardName.textAlign = "center";
         this.cardName.font = "8.5px Arial";
         this.cardName.x = 42;
-        this.cardName.y = 4;
+        this.cardName.y = 5;
+        this.cardCap = new createjs.Text(null);
+        this.cardCap.x = 4;
+        this.cardCap.y = 69;
+        this.cardCap.font = "5.4px Arial";
+        this.cardCap.lineHeight = 5.4;
+        this.cardLevel = new createjs.Text(null);
+        this.cardLevel.font = "7px Arial";
+        this.cardLevel.x = 4;
+        this.cardLevel.y = 17;
+        this.cardType = new createjs.Text(null);
+        this.cardType.font = "6.4px Arial";
+        this.cardType.x = 56;
+        this.cardType.y = 17;
+
         this.addChild(this.cardFrame);
         this.addChild(this.cardImage);
         this.addChild(this.cardName);
+        this.addChild(this.cardCap);
+        this.addChild(this.cardLevel);
+        this.addChild(this.cardType);
+        
         this.cardFrame.addEventListener("click", () => {
             if (this.yamlData != null)
                 this.onClickCallBack(index, this.yamlData.name);
         });
         this.cardFrame.addEventListener("mouseover", () => { 
+            this.stage.update();
         });
         this.cardFrame.addEventListener("mouseout", () => {
-
+            this.stage.update();
         });
 
     }
@@ -48,6 +70,9 @@ export class ActionStorageCard extends createjs.Container {
             this.cardFrame.image = new createjs.Bitmap(queue.getResult("f_level" + yamlData.level)).image;
             this.cardImage.image = new createjs.Bitmap(queue.getResult(yamlData.name)).image;
             this.cardName.text = yamlData.name;
+            this.cardCap.text = yamlData.description;
+            this.cardLevel.text = "LEVEL " + yamlData.level;
+            this.cardType.text = yamlData.build_use ? "設置使用" : "使い切り";
         } else {
             //ここは手札がないことを表すので、画像はすべてなくしておく
             this.cardFrame.image = null;
