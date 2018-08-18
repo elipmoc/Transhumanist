ef40cb2958b6db773356eb2c53e8f91cd34f3f0d
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-08-18 23:09:02 +0900

    [update]アイコン表示の為の色々を作った。

--- Client/boardView/actionCardHover.ts
+++ Client/boardView/actionCardHover.ts
@@ -1,11 +1,13 @@
 import { ActionCardYamlData } from "../../Share/Yaml/actionCardYamlData";
 import { MakeCard } from "../../Client/boardView/makeCard";
+import { getIconResource } from "../utility";
 import * as global from "../boardGlobalData";
 
 export class ActionCardHover extends createjs.Container {
     private cardInfo: MakeCard;
     private backGround: createjs.Shape;
-
+    private costIcon: createjs.Bitmap[] = [null,null,null,null,null];
+        
     readonly cardWidth: number = 253;
     readonly cardHeight: number = 379;
 
@@ -19,11 +21,26 @@ export class ActionCardHover extends createjs.Container {
 
         this.addChild(this.backGround);
         this.addChild(this.cardInfo);
+
+        for (let i = 0; i < 5; i++) {
+            if (this.costIcon[i] != null) {
+                this.addChild(this.costIcon[i]);
+            }
+        }
+
         this.x = global.canvasWidth / 2 - this.cardWidth / 2;
         this.y = global.canvasHeight / 2 - this.cardHeight / 2;
 
     }
     setYamlData(yamlData: ActionCardYamlData | null, queue: createjs.LoadQueue) {
         this.cardInfo.setYamlData(yamlData, queue);
+
+        for (let i = 0; i < 5; i++){
+            console.log(yamlData.cost[i]);
+            if (yamlData.cost[i] != null) {
+                this.costIcon[i].image = getIconResource(yamlData.index, "resource", queue);
+            }
+            else break;
+        }
     }
 }
