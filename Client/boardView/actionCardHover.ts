340d6ebab1c0476987261a44d1a141d8db1a6fca
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-08-19 00:19:15 +0900

    [update]actionCardHoverを色々した。（まだおかしい）

--- Client/boardView/actionCardHover.ts
+++ Client/boardView/actionCardHover.ts
@@ -1,32 +1,36 @@
 import { ActionCardYamlData } from "../../Share/Yaml/actionCardYamlData";
 import { MakeCard } from "../../Client/boardView/makeCard";
-import { getIconResource } from "../utility";
 import * as global from "../boardGlobalData";
+import { ResourceHash } from "../../Share/Yaml/resourceYamlData";
+import { getIconResource } from "../utility";
 
 export class ActionCardHover extends createjs.Container {
     private cardInfo: MakeCard;
     private backGround: createjs.Shape;
-    private costIcon: createjs.Bitmap[] = [null,null,null,null,null];
-        
+    private resourceHash: ResourceHash;
+    private costIcon: createjs.Bitmap[] = [new createjs.Bitmap(null), new createjs.Bitmap(null), new createjs.Bitmap(null), new createjs.Bitmap(null), new createjs.Bitmap(null)];
+
     readonly cardWidth: number = 253;
     readonly cardHeight: number = 379;
 
-    constructor(yamlData: ActionCardYamlData, queue: createjs.LoadQueue, size: number) {
+    constructor(resourceHash: ResourceHash, queue: createjs.LoadQueue, size: number) {
         super();
+        this.resourceHash = resourceHash;
         this.cardInfo = new MakeCard(size);
-        this.cardInfo.setYamlData(yamlData, queue);
-
         this.backGround = new createjs.Shape();
         this.backGround.graphics.beginFill("#EEE").drawRect(-7, -7, (this.cardWidth) + 14, (this.cardHeight) + 14);
 
         this.addChild(this.backGround);
         this.addChild(this.cardInfo);
-
-        for (let i = 0; i < 5; i++) {
-            if (this.costIcon[i] != null) {
+        for (let i = 0; i < global.costCountMax; i++) {
+            if (this.costIcon[i].image != null) {
+                this.costIcon[i].x = 0;
+                this.costIcon[i].y = 40;
                 this.addChild(this.costIcon[i]);
             }
+            else break;
         }
+
 
         this.x = global.canvasWidth / 2 - this.cardWidth / 2;
         this.y = global.canvasHeight / 2 - this.cardHeight / 2;
@@ -34,11 +38,10 @@ export class ActionCardHover extends createjs.Container {
     }
     setYamlData(yamlData: ActionCardYamlData | null, queue: createjs.LoadQueue) {
         this.cardInfo.setYamlData(yamlData, queue);
-
-        for (let i = 0; i < 5; i++){
-            console.log(yamlData.cost[i]);
-            if (yamlData.cost[i] != null) {
-                this.costIcon[i].image = getIconResource(yamlData.index, "resource", queue);
+        
+        for (let i = 0; i < global.costCountMax; i++){
+            if (yamlData != null && yamlData.cost.length > i) {
+                this.costIcon[i].image = getIconResource(this.resourceHash[yamlData.cost[i].name].index, "resource", queue);
             }
             else break;
         }
