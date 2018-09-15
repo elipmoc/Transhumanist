01b38be0509016a2efb187402bd48908b375de1a
elipmoc <relipmoc101@gmail.com>
2018-09-15 15:36:28 +0900

    [update]リソースを捨てるダイアログを表示

--- Client/board/views/handActionCard/actionCardUseDecisionWindow.ts
+++ Client/board/views/handActionCard/actionCardUseDecisionWindow.ts
@@ -1,21 +1,6 @@
 import { global } from "../../../boardGlobalData";
 import { createMyShadow } from "../../../utility";
-
-class DecisionButton extends createjs.Container {
-    readonly width = 150;
-    readonly height = 50;
-    constructor(_text: string) {
-        super();
-        const background = new createjs.Shape(new createjs.Graphics().beginFill("white").drawRect(-this.width / 2, -this.height / 2, this.width, this.height));
-        const text = new createjs.Text(_text);
-        text.color = "black";
-        text.textAlign = "center";
-        text.font = "32px Bold ＭＳ ゴシック";
-        text.y = -20;
-        this.addChild(background);
-        this.addChild(text);
-    }
-}
+import { DecisionButton } from "../decisionButton";
 
 export const enum DialogResult {
     Yes,
---
+++ Client/board/views/resourceDialog.ts
@@ -0,0 +1,34 @@
+import { global } from "../../boardGlobalData";
+import { createMyShadow } from "../../utility";
+import { DecisionButton } from "./decisionButton";
+
+//リソースがいっぱいでなんか捨てないといけない時のダイアログ
+export class ResourceDialog extends createjs.Container {
+    private label: createjs.Text;
+    private callBack: () => void;
+    onClick(callBack: () => void) {
+        this.callBack = callBack;
+    }
+    constructor() {
+        super();
+        const width = 500;
+        const height = 250;
+        const background = new createjs.Shape(new createjs.Graphics().beginFill("gray")
+            .drawRect(global.canvasWidth / 2 - width / 2, global.canvasHeight / 2 - height / 2, width, height));
+        this.label = new createjs.Text("undefined");
+        this.label.font = "32px Bold ＭＳ ゴシック";
+        this.label.color = "white";
+        this.label.shadow = createMyShadow();
+        this.label.textAlign = "center";
+        this.label.x = global.canvasWidth / 2;
+        this.label.y = global.canvasHeight / 2 - 100;
+        const button = new DecisionButton("決定");
+        button.x = global.canvasWidth / 2;
+        button.y = global.canvasHeight / 2 + height / 2 - 30 - button.height / 2;
+        button.addEventListener("click", () => this.callBack());
+        this.addChild(background, this.label, button);
+    }
+    setThrowResourceNum(num: number) {
+        this.label.text = `リソースがいっぱいです。\n捨てるリソースを\n${num}個選んでください`;
+    }
+}
