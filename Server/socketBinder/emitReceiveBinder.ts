a5a44035f4b216b4707ad911240ff6a5ad351742
elipmoc <relipmoc101@gmail.com>
2018-09-24 16:46:28 +0900

    [update]EmitReceiveBinderの実装

---
+++ Server/socketBinder/emitReceiveBinder.ts
@@ -0,0 +1,48 @@
+import { BinderBase } from "./binderBase";
+
+//メッセージをクライアントから受け取るクラス
+export class EmitReceiveBinder<T> implements BinderBase {
+
+    private valueName: string;
+    private privateFlag: boolean;
+    private privateSocketTags: Array<string>;
+    private callback: (t: T) => void;
+
+    constructor(
+        valueName: string,
+        privateFlag: boolean = false,
+        privateSocketTags: string[] = []
+    ) {
+        this.valueName = valueName;
+        this.privateFlag = privateFlag;
+        this.privateSocketTags = privateSocketTags;
+    }
+
+    get ValueName() {
+        return this.valueName;
+    }
+
+    OnReceive(callback: (t: T) => void) {
+        this.callback = callback;
+    }
+
+    setNamespace(namespace: SocketIO.Namespace) { }
+
+    private setReceive(socket: SocketIO.Socket) {
+        socket.on(this.valueName, (data) =>
+            this.callback(JSON.parse(data))
+        );
+    }
+
+    connect(tag: string, socket: SocketIO.Socket) {
+        if (this.privateFlag == false) {
+            this.setReceive(socket);
+            return;
+        }
+        if (this.privateSocketTags.find(x => x == tag) != undefined) {
+            this.setReceive(socket);
+        }
+    }
+
+
+}
