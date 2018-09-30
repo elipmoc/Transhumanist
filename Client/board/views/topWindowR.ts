dffe515ed2cf108f409ebc13d3eaf8f25d076ff4
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-09-30 17:09:42 +0900

    [updata]イベントカードの枚数を表示

--- Client/board/views/topWindowR.ts
+++ Client/board/views/topWindowR.ts
@@ -7,6 +7,7 @@ export class TopWindowR extends createjs.Container {
     private eventTitle: createjs.Text;
     private eventLevel: createjs.Text;
     private textEvent: createjs.Text;
+    private currentNum: createjs.Text;
 
     private levelColor: string[] = ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#a4c2f4"];
 
@@ -24,6 +25,9 @@ export class TopWindowR extends createjs.Container {
         this.textEvent = new createjs.Text("EVENT", "18px Arial");
         this.textEvent.color = "white";
 
+        this.currentNum = new createjs.Text("", "18px Arial");
+        this.currentNum.color = "white";
+
         //this.eventTitle.textAlign = "center";
         //this.eventTitle.regY = this.eventTitle.getMeasuredHeight() / 2;
         this.eventTitle.x = topWindowsRFrame.x - topWindowsRFrame.image.width / 2 - 15;
@@ -32,10 +36,16 @@ export class TopWindowR extends createjs.Container {
         this.eventLevel.y = topWindowsRFrame.y + topWindowsRFrame.image.height / 2 + 5;
         this.textEvent.x = topWindowsRFrame.x - topWindowsRFrame.image.width / 2 - 90;
         this.textEvent.y = topWindowsRFrame.y + topWindowsRFrame.image.height / 2 - 25;
+        this.currentNum.x = topWindowsRFrame.x - topWindowsRFrame.image.width / 2 - 20;
+        this.currentNum.y = topWindowsRFrame.y + topWindowsRFrame.image.height / 2 - 25;
 
         this.addChild(this.eventTitle);
         this.addChild(this.eventLevel);
         this.addChild(this.textEvent);
+        this.addChild(this.currentNum);
+    }
+    setEventNum(num: string){
+        this.currentNum.text = num;
     }
     setEventName(event: Event) {
         this.eventTitle.text = event.name;
