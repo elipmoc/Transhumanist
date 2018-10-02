130f9e551457d83295e755305594874e8f9d1132
elipmoc <relipmoc101@gmail.com>
2018-10-02 15:26:47 +0900

    [fix]soundmanagerのバグを修正

--- Client/board.ts
+++ Client/board.ts
@@ -71,7 +71,6 @@ window.onload = () => {
 }
 
 function preloadImage(yamls: Yamls, spriteJson: any) {
-    SoundManager.bgmPlay("bgm_level3");
     let stage = new createjs.Stage("myCanvas");
     stage.enableMouseOver();
     let background = new BackGround();
@@ -92,6 +91,6 @@ function preloadImage(yamls: Yamls, spriteJson: any) {
     }
     viewBuild(bindParams);
     let bgmChanger = new BgmChanger(bindParams);
-        
+
     stage.update();
 }
