4475e55105651462e86af9b95e83c438f41f618f
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-10-04 23:27:57 +0900

    [update]イベントカードの情報をちゃんと表示できるようにした。

--- Server/boardGame/drawCard/eventCardStack.ts
+++ Server/boardGame/drawCard/eventCardStack.ts
@@ -3,9 +3,13 @@ import { GenerateEventYamlDataArray, Event } from "../../../Share/Yaml/eventYaml
 import { arrayshuffle } from "../../../Share/utility";
 import { NumberOfEventCard } from "../../../Share/numberOfEventCard";
 import { SocketBinder } from "../../socketBinder";
+import { EventLogMessageForClient } from "../../../Share/eventLogMessageForClient";
+
 export class EventCardStack {
     private eventCardList: Event[] = [];
     private numberOfEventCard: SocketBinder.Binder<NumberOfEventCard>;
+    private eventLogMessage: SocketBinder.Binder<EventLogMessageForClient>;
+
     constructor(boardSocketManager: SocketBinder.Namespace) {
         const eventCardGroups = new Array(6);
         for (let i = 0; i < 6; i++) {
@@ -23,13 +27,24 @@ export class EventCardStack {
             currentNumber: this.eventCardList.length,
             maxNumber: this.eventCardList.length
         }
-        boardSocketManager.addSocketBinder(this.numberOfEventCard);
+
+        this.eventLogMessage = new SocketBinder.Binder<EventLogMessageForClient>("eventLogMessage");
+        this.eventLogMessage.Value = new EventLogMessageForClient("", "");
+
+        boardSocketManager.addSocketBinder(this.numberOfEventCard, this.eventLogMessage);
     }
 
     draw() {
         const drawEvent = this.eventCardList.pop();
         this.numberOfEventCard.Value.currentNumber = this.eventCardList.length
         this.numberOfEventCard.update();
+        this.setEventMessage(drawEvent);
         return drawEvent;
+    }
+
+    setEventMessage(event: Event | undefined) {
+        if (event != undefined) {
+            this.eventLogMessage.Value = new EventLogMessageForClient(`イベント【${event.name}】が発生しました`,event.description);
+        }
     }
 }
