4c0082de01fbf7c9d8d6fc37bd38b2a48e1f93c2
elipmoc <relipmoc101@gmail.com>
2018-11-24 01:43:42 +0900

    [refactoring & fix]リファクタリング&コードが動かないバグを修正

--- Server/boardGame/war.ts
+++ Server/boardGame/war.ts
@@ -1,72 +1,43 @@
 import { SocketBinder } from "../socketBinder";
 
-export class War {
+export type WarSuccessFlag = boolean;
 
-    private declareWar: SocketBinder.EmitReceiveBinder<(number | null)[]>;
-    private warPairList: SocketBinder.BinderList<{ playerId1: number, playerId2: number }>;
-    private warFlags: SocketBinder.Binder<boolean>[] = [];
-    private winCallback: (playerId: number) => void;
-    private loseCallback: (playerId: number) => void;
-    private startWarCallback: (playerId: number) => void;
+export class War {
+    private surrenderCallback: () => WarSuccessFlag;
+    private startWarCallback: (targetPlayerId: number) => WarSuccessFlag;
+    private warFlag = false;
+
+    getWarFlag() { return this.warFlag; }
+
+    win() {
+        this.warFlag = false;
+    }
 
     reset() {
-        this.warPairList.Value = [];
-        this.warFlags.forEach(x => x.Value = false);
+        this.warFlag = false;
     }
 
-    constructor(boardSocketManager: SocketBinder.Namespace) {
-        this.declareWar = new SocketBinder.EmitReceiveBinder<number[]>("declareWar");
-        this.declareWar.OnReceive(x => {
-            if (
-                x.length == 2 &&
-                x[0] != null &&
-                x[1] != null &&
-                x[0] != x[1]
-            ) {
-                const same = this.warPairList.Value.find(y =>
-                    y.playerId1 == x[0] || y.playerId2 == x[0] || y.playerId1 == x[1] || y.playerId2 == x[1]
-                );
-                if (same == undefined) {
-                    this.warFlags[x[0]!].Value = true;
-                    this.warFlags[x[1]!].Value = true;
-                    this.startWarCallback(x[0]!);
-                    this.startWarCallback(x[1]!);
-                    this.warPairList.push({ playerId1: x[0]!, playerId2: x[1]! });
-                }
+    constructor(boardSocketManager: SocketBinder.Namespace, playerId: number) {
+        const declareWar = new SocketBinder.EmitReceiveBinder<number | null>("declareWar", true, [`player${playerId}`]);
+        declareWar.OnReceive(x => {
+            if (x && this.warFlag == false && this.startWarCallback(x)) {
+                this.warFlag = true;
             }
         });
+        const surrender = new SocketBinder.EmitReceiveBinder("surrender", true, [`player${playerId}`]);
 
-        this.warPairList = new SocketBinder.BinderList<{ playerId1: number, playerId2: number }>("warPairList");
-        this.warPairList.Value = [];
-        boardSocketManager.addSocketBinder(this.declareWar, this.warPairList);
-        for (let i = 0; i < 4; i++) {
-            const warFlag = new SocketBinder.Binder<boolean>("warFlag", true, [`player${i}`]);
-            warFlag.Value = false;
-            this.warFlags.push(warFlag);
-            const surrender = new SocketBinder.EmitReceiveBinder("surrender", true, [`player${i}`]);
-            const playerId = i;
-            surrender.OnReceive(() => {
-                if (this.warFlags[playerId].Value) {
-                    const warPair = this.warPairList.Value.find(x => x.playerId1 == playerId || x.playerId2 == playerId)!;
-                    this.warFlags[warPair.playerId1].Value = false;
-                    this.warFlags[warPair.playerId2].Value = false;
-                    this.loseCallback(playerId);
-                    this.winCallback(playerId == warPair.playerId1 ? warPair.playerId2 : warPair.playerId1);
-                    this.warPairList.Value = this.warPairList.Value.filter(x => x.playerId1 != playerId && x.playerId2 != playerId);
-                }
-            });
-            boardSocketManager.addSocketBinder(warFlag, surrender);
-        }
+        surrender.OnReceive(() => {
+            if (this.warFlag && this.surrenderCallback()) {
+                this.warFlag = false;
+            }
+        });
+        boardSocketManager.addSocketBinder(declareWar, surrender);
     }
 
-    onWin(f: (playerId: number) => void) {
-        this.winCallback = f;
+    onSurrender(f: () => WarSuccessFlag) {
+        this.surrenderCallback = f;
     }
-
-    onLose(f: (playerId: number) => void) {
-        this.loseCallback = f;
-    }
-    onStartWar(f: (playerId: number) => void) {
+    onStartWar(f: (playerId: number) => WarSuccessFlag) {
         this.startWarCallback = f;
     }
 }
