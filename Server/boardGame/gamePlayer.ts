c93aa54265e402ae70125524552c4638553cfddb
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-12-08 01:38:16 +0900

    [add]即時仕様アクションの効果を実装（非戦争、非勝利カード）

--- Server/boardGame/gamePlayer.ts
+++ Server/boardGame/gamePlayer.ts
@@ -333,10 +333,57 @@ export class GamePlayer {
                     unavailable.emit(UnavailableState.War);
                     return false;
                 }
-                if (card.build_use)
-                    this.buildActionList.addBuildAction(card.name);
                 if (this.onceNoCostFlag)
                     this.onceNoCostFlag = false;
+                
+                //ここまでイベント
+                if (card.build_use)
+                    this.buildActionList.addBuildAction(card.name);
+                else {
+                    switch (card.name) {
+                        case "ロケットの開発":
+                            this.resourceList.addResource("ロケット");
+                            break;
+                        case "花火大会":
+                            //P点がリソース内の人間の半分（切り捨て）の数増える
+                            break;
+                        case "布教活動":
+                            this.resourceList.changeResource("人間","信者",1);
+                            break;
+                        case "火星探査":
+                            this.resourceList.addResource("火星の情報");
+                            break;
+                        case "ミサイル発射":
+                            //戦争相手の設置済みアクションを1つ破壊。
+                            break;
+                        case "衛星の打ち上げ":
+                            this.resourceList.addResource("衛星");
+                            break;
+                        case "チップの埋め込み":
+                            this.resourceList.addResource("拡張人間");
+                            break;
+                        case "細菌兵器":
+                            //戦争相手のリソースにある人間を、サイコロの数分、病人に変える。
+                            break;
+                        case "テラフォーミング":
+                            this.resourceList.addResource("テラフォーミング");
+                            break;
+                        case "神の杖":
+                            //戦争相手の設置アクションを1つと、リソースをサイコロの数分破壊する。
+                            break;
+                        case "意識操作のテスト":
+                            this.state.addNegative(-1);
+                            this.state.addPositive(1);
+                            break;
+                        case "御神体の再生":
+                            this.resourceList.addResource("神体");
+                            break;
+                        case "火星の支配": case "A.Iによる支配": case "宗教による支配":
+                            //勝利カードの処理
+                            break;
+                    }
+                }
+                
                 return true;
             }
         );
