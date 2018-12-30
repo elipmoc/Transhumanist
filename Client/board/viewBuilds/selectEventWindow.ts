f37938a5faa87dafa9cd0be4bd11c191de4b0690
Sutaa0101 <sutaahbts@yahoo.co.jp>
2018-12-31 00:14:46 +0900

    [update]個数対応。動かなくても知らん

--- Client/board/viewBuilds/selectEventWindow.ts
+++ Client/board/viewBuilds/selectEventWindow.ts
@@ -1,18 +1,24 @@
 import { SelectEventWindow } from "../views/selectEventWindow";
 import { BindParams } from "../bindParams";
-import { DiceData } from "../../../Share/diceData";
+import { FutureForecastEventData } from "../../../Share/futureForecastEventData";
 import { SocketBinder } from "../../socketBinder";
 import { LayerTag } from "../../board";
 import { GamePlayerCondition } from "../../../Share/gamePlayerCondition";
 
 //イベントカードウインドウの生成
 export function build(bindParams: BindParams) {
-    const diceData = new SocketBinder<DiceData>("diceList" + bindParams.playerId, bindParams.socket);
-    const selectEventWindow = new SelectEventWindow(()=>{bindParams.layerManager.update()});
+    //const diceData = new SocketBinder<DiceData>("diceList" + bindParams.playerId, bindParams.socket);
+    const selectEventWindow = new SelectEventWindow(() => { bindParams.layerManager.update() });
     bindParams.layerManager.add(LayerTag.PopUp, selectEventWindow);
     //selectEventWindow.visible = false;
 
-    selectEventWindow.submitOnClick((list:number[]) => { 
+    const data: FutureForecastEventData = {
+        eventNameList: ["無風状態", "ムーアの法則", "ブラックホールの発生"]
+    }
+
+    selectEventWindow.setData(data.eventNameList);
+    bindParams.layerManager.update();
+    selectEventWindow.submitOnClick((list:string[]) => { 
         console.log(list);
     });
     
