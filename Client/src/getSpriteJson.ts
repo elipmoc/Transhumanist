0748329e7e19a6e67b0816ca48913d4b0cc3fa13
elipmoc <relipmoc101@gmail.com>
2019-11-16 17:58:30 +0900

    これで動くはず

--- Client/src/getSpriteJson.ts
+++ Client/src/getSpriteJson.ts
@@ -2,11 +2,7 @@ import * as $ from "jquery";
 
 export async function getSpriteJson(): Promise<any> {
     const json = await new Promise<any>((resolve) =>
-        $.get("./Resource/Sprite/boardSprite.json", (data: string) => {
-            console.log(data); resolve(
-                JSON.parse(data)
-            )
-        })
+        $.get("./Resource/Sprite/boardSprite.json", (data: string) => resolve(data))
     );
     return json;
 }
